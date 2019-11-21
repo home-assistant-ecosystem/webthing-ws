ce3b9b1ecd07bf2dfd3c5b42836b7729948e9d6e
Fabian Affolter <mail@fabian-affolter.ch>
2019-11-21 21:28:58 +0100

    Add setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""Setup file for WebThing WS."""
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as readme:
+    long_description = readme.read()
+
+if sys.argv[-1] == 'publish':
+    os.system('python3 setup.py sdist upload')
+    sys.exit()
+
+setup(
+    name='webthing-ws-api',
+    version='0.1.0',
+    description='A WebThing WebSocket consumer and API client.',
+    long_description=long_description,
+    url='https://github.com/fabaff/webthing-ws',
+    download_url='https://github.com/fabaff/webthing-es/releases',
+    author='Fabian Affolter',
+    author_email='fabian@affolter-engineering.ch',
+    license='MIT',
+    install_requires=['aiohttp', 'async_timeout'],
+    packages=['webthing_ws'],
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Utilities',
+    ],
+)
