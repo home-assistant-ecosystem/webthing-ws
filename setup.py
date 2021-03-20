370657bde2eaa573e8a38c9e72d450516fb6af21
Fabian Affolter <mail@fabian-affolter.ch>
2021-03-20 21:05:03 +0100

    Formatting with black

--- setup.py
+++ setup.py
@@ -10,36 +10,36 @@ except ImportError:
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, 'README.rst'), encoding='utf-8') as readme:
+with open(os.path.join(here, "README.rst"), encoding="utf-8") as readme:
     long_description = readme.read()
 
-if sys.argv[-1] == 'publish':
-    os.system('python3 setup.py sdist upload')
+if sys.argv[-1] == "publish":
+    os.system("python3 setup.py sdist upload")
     sys.exit()
 
 setup(
-    name='webthing-ws',
-    version='0.1.0',
-    description='A WebThing WebSocket consumer and API client.',
+    name="webthing-ws",
+    version="0.1.0",
+    description="A WebThing WebSocket consumer and API client.",
     long_description=long_description,
-    url='https://github.com/home-assistant-ecosystem/webthing-ws',
-    download_url='https://github.com/home-assistant-ecosystem/webthing-ws/releases',
-    author='Fabian Affolter',
-    author_email='fabian@affolter-engineering.ch',
-    license='MIT',
-    install_requires=['aiohttp>=3.7.4,<4', 'async_timeout<4'],
-    packages=['webthing_ws'],
+    url="https://github.com/home-assistant-ecosystem/webthing-ws",
+    download_url="https://github.com/home-assistant-ecosystem/webthing-ws/releases",
+    author="Fabian Affolter",
+    author_email="fabian@affolter-engineering.ch",
+    license="MIT",
+    install_requires=["aiohttp>=3.7.4,<4", "async_timeout<4"],
+    packages=["webthing_ws"],
     zip_safe=True,
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Topic :: Utilities',
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Utilities",
     ],
 )
