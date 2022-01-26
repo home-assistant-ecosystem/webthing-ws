72c457085b8ff9283e498d49d9f33412cf07e808
Fabian Affolter <mail@fabian-affolter.ch>
2022-01-26 16:15:17 +0100

    Prepare for 0.1.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(here, "README.rst"), encoding="utf-8") as readme:
 
 setup(
     name="webthing-ws",
-    version="0.1.1",
+    version="0.1.2",
     description="A WebThing WebSocket consumer and API client.",
     long_description=long_description,
     url="https://github.com/home-assistant-ecosystem/webthing-ws",
