bc3a3e4a95fb59200e3c74db91b1d20b7eca42aa
Fabian Affolter <mail@fabian-affolter.ch>
2026-04-16 11:09:02 +0200

    Update aiohttp version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     author_email="fabian@affolter-engineering.ch",
     license="MIT",
     install_requires=[
-        "aiohttp>=3.8.0,<4",
+        "aiohttp>=3.13.0,<4",
         "async_timeout",
     ],
     packages=["webthing_ws"],
